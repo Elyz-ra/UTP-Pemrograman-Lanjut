706ffc21e97935738aff9269b786ac7d575a48c8
Elyzia Janara <103510445+Elyz-ra@users.noreply.github.com>
2022-06-18 15:37:59 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,77 +1,24 @@
 package com.company;
+import java.util.ArrayList;
 
 public class Main {
-
-    static Manusia manusia[];
-    static MahasiswaFILKOM mahasiswa[];
-    static Pekerja pekerja[];
-    static Manager manager[];
-
     public static void main(String[] args) {
-        //Class Manusia
-       manusia = new Manusia[3];
+        ArrayList<Pegawai> pegawai = new ArrayList<>();
 
-       System.out.printf("%35s\n","Class Manusia");
-        System.out.println("===========================================================");
+        pegawai.add(new PegawaiTetap("Bayu", "350728490327424892342", 2000000));
+        pegawai.add(new PegawaiTetap("Keqing", "350728490327424892345", 5000000));
+        pegawai.add(new PegawaiTetap("Thoma", "350728490327424892348", 2500000));
 
-        manusia[0] = new Manusia("Sangonomiya Kokomi", "666555444222", false, false);
-        manusia[1] = new Manusia("Albedo Kreidenprinze", "888444666111", true, false);
-        manusia[2] = new Manusia("Kujou Sara", "999666333777", false, true);
+        pegawai.add(new PegawaiHarian("Edo", "350728490327424892343", 8500, 40));
+        pegawai.add(new PegawaiHarian("Diluc", "350728490327424892346", 9000, 42));
+        pegawai.add(new PegawaiHarian("Ningguang", "350728490327424892349", 10000, 44));
 
-        System.out.println(manusia[0].toString());
-        System.out.println(manusia[1].toString());
-        System.out.println(manusia[2].toString());
+        pegawai.add(new Sales("Tika", "350728490327424892344", 50, 50000));
+        pegawai.add(new Sales("Jean", "350728490327424892347", 55, 55000));
+        pegawai.add(new Sales("Sayu", "350728490327424892350", 60, 60000));
 
-        //Class MahasiswaFILKOM
-        mahasiswa = new MahasiswaFILKOM[3];
-
-        System.out.printf("\n%35s\n","Class Mahasiswa");
-        System.out.println("===========================================================");
-
-        mahasiswa[0] = new MahasiswaFILKOM("215150707111024", 3.9, "Elyzia Janara", "123456789123", false, false);
-        mahasiswa[1] = new MahasiswaFILKOM("202090300911002", 3.8, "Xiao", "987654321987", true, false);
-        mahasiswa[2] = new MahasiswaFILKOM("202150406611999", 3.3, "Eula Lawrence", "321456987452", false, true);
-
-        System.out.println(mahasiswa[0].toString());
-        System.out.println(mahasiswa[1].toString());
-        System.out.println(mahasiswa[2].toString());
-
-        //Class Pekerja
-        pekerja = new Pekerja[4];
-
-        System.out.printf("\n%35s\n", "Class Pekerja ");
-        System.out.println("===========================================================");
-
-        pekerja[0] = new Pekerja(9, 30, "314127229647", "Yae Miko", "666777888999", false, true);
-        pekerja[1] = new Pekerja(8, 29, "323325329957", "Kamisato Ayaka", "111222333444", false, true);
-        pekerja[2] = new Pekerja(7, 28, "3322256291648", "Kaedehara Kazuha", "444333222111", true, false);
-        pekerja[3] = new Pekerja(6, 27, "1412259291648", "Kaeya Alberich", "999888777666", true, false);
-
-        System.out.println(pekerja[0].toString());
-        System.out.println(pekerja[1].toString());
-        System.out.println(pekerja[2].toString());
-        System.out.println(pekerja[3].toString());
-
-        //Class Manager
-        manager = new Manager[3];
-
-        System.out.printf("\n%35s\n","Class Manager");
-        System.out.println("===========================================================");
-
-        manager[0] = new Manager(3000, 6, 30, "301095729156", "Kamisato Ayato", "222000777999", true, false);
-        manager[1] = new Manager(2000, 7, 24, "102215820906", "Diluc Ragnvindr", "666444222111", true, true);
-        manager[2] = new Manager(1000, 8, 20, "103295521236", "Jean Gunnhildr", "999333000444", false, true);
-
-        System.out.println(manager[0].toString());
-        System.out.println(manager[1].toString());
-        System.out.println(manager[2].toString());
-
-        //Jumlah
-        System.out.printf("\n%45s\n","Total Keseluruhan yang Terdaftar ");
-        System.out.println("===========================================================");
-        System.out.println("Ada " + manusia.length +" Manusia");
-        System.out.println("Ada " + mahasiswa.length +" Mahasiswa Aktif");
-        System.out.println("Ada " + pekerja.length +" Pekerja Aktif");
-        System.out.println("Ada " + manager.length +" Manager Aktif");
+        for (int i = 0; i < pegawai.size(); i++){
+            System.out.println(pegawai.get(i).toString());
+        }
     }
 }
